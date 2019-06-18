cf4373dcc7d1c3bae269e79583f94cfedc6b8576
abdulkadirhaci <786bozkurt@gmail.com>
2019-06-18 16:04:38 -0400

    adding my codes to the github

---
+++ src/MyNewPackage/MyNewCode.java
@@ -0,0 +1,20 @@
+package MyNewPackage;
+
+public class MyNewCode {
+
+    public static void main(String[] args) {
+
+
+
+        System.out.println(" Welcome to Intellij ! ");
+
+        System.setProperty("webdriver.chrome.driver",
+                "/Users/abdulkadirhaci/Documents/My_IT_Resources/Drivers/chromedriver");
+
+
+
+
+
+    }
+
+}
